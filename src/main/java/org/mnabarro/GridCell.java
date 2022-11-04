14de35fa2c5ada785c98e2e5fd32637a44b7704c
Matias Nabarro <matiasnabarro@gmail.com>
2022-11-04 20:51:33 +0100

    feat: methods for GridCell class

--- src/main/java/org/mnabarro/GridCell.java
+++ src/main/java/org/mnabarro/GridCell.java
@@ -1,4 +1,17 @@
 package org.mnabarro;
 
 public class GridCell {
+    boolean locked;
+
+    public GridCell(boolean locked) {
+        this.locked = locked;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
 }
